bc607ca47887f6ada3b5187b5f0cbf098b41a2ab
John Houston <jhouston@hashicorp.com>
2022-11-14 17:23:48 -0500

    GKE example

--- variables.tf
+++ variables.tf
@@ -1,5 +1,24 @@
-variable "replicas" {
+variable "pool_name" {
   type = string
-  description = "Number of replicas the machine pool should provision"
-  default = 1
+
+  description = "The machine pool name"
+}
+
+variable "cluster_name" {
+  type = string
+
+  description = "The cluster name"
+}
+
+variable "replicas" {
+  type = number
+
+  description = "The number of replicas in the machine pool"
+}
+
+variable "machine_type" {
+  type    = string
+  default = "e2-standard-2"
+
+  description = "The type of machines in the machine pool"
 }
